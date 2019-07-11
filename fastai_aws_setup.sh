8a33e7022fef61727d573016dab5732f763b0a9f
Vamsavardhan Thotakura <vamsavardhan.thotakura@gmail.com>
2019-07-11 14:56:04 +0530

    steps to install fastai libraries in  aws instances

---
+++ fastai_aws_setup.sh
@@ -0,0 +1,23 @@
+
+
+# Install Kaggle API
+pip install kaggle
+
+# TODO : Setup JSON for kaggle command 
+# Do it manually 
+
+# TODO : setup github access with key 
+
+
+# Activate desired env pytorch with python 3
+source activate pytorch_p36
+
+# install pytorch 1.1.0 version
+conda install -y pytorch=1.1.0 torchvision cudatoolkit -c pytorch
+
+# install fast ai 
+# -y for no prompt
+conda install -y -c pytorch -c fastai fastai
+
+# Launch jupyter 
+jupyter notebook --ip=0.0.0.0 --no-browser 
